c0289978bbcab3fbb258068f477d4e5dd59467a8
vitali <vitali@gmail.com>
2024-03-04 02:28:57 +0300

    lock providers versions

--- ArgoCD/ArgoCD-k8s-project/00-settings.tf
+++ ArgoCD/ArgoCD-k8s-project/00-settings.tf
@@ -1,7 +1,7 @@
 # https://developer.hashicorp.com/terraform/language/settings/backends/gcs
 
 terraform {
-  required_version = "1.6.2"
+  required_version = "~> 1.6.2"
 
   backend "gcs" {
     bucket = "argocd-k8s-project-bucket-tfstate"
@@ -11,23 +11,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.39.0"
+      version = "~> 5.39.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "5.18.0"
+      version = "~> 5.18.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "1.14.0"
+      version = "~> 1.14.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.12"
+      version = "~> 2.12"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.26.0"
+      version = "~> 2.26.0"
     }
   }
 }
